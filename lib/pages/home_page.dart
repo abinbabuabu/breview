fac409b1ef21e08bb451b1c76976821398362df2
Soujanya22 <soujanya.m.abbigeri@gmail.com>
2021-12-15 22:08:14 +0530

    Added home page code

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:breview/pages/blogs_page.dart';
+import 'package:breview/pages/friendslist_page.dart';
+import 'package:breview/util/Constants.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> _children = [
+      BlogsPage(),
+      FriendsList(),
+    ];
+
+    void _onTapped(int index) {
+      setState(() {
+        _currentIndex = index;
+      });
+    }
+
+    return FlutterWebFrame(
+        maximumSize: Size(750, double.infinity),
+        enabled: kIsWeb && MediaQuery.of(context).size.width > 700,
+        backgroundColor: Colors.grey,
+        builder: (context) {
+          return Scaffold(
+              body: _children[_currentIndex],
+              bottomNavigationBar: BottomNavigationBar(
+                backgroundColor: Colors.black,
+                selectedItemColor: Constants.SECONDARY_COLOR,
+                currentIndex: _currentIndex,
+                onTap: _onTapped,
+                items: [
+                  BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+
+                ],
+              ));
+        }
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+}
