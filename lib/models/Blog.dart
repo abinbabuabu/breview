01337ed886b612e5f8fcc50d0168d30562e180b2
Soujanya22 <soujanya.m.abbigeri@gmail.com>
2021-12-16 18:17:15 +0530

    Added widget files and model files

---
+++ lib/models/Blog.dart
@@ -0,0 +1,11 @@
+class Blog {
+  String userName;
+  String profilePictureUrl;
+  int likes;
+  bool isLiked;
+  String content;
+  String image;
+
+  Blog(this.userName, this.profilePictureUrl, this.likes, this.isLiked,
+      this.content, this.image);
+}
