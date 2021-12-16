60e0918cc0921c19190a66d8ad2f566f2aa0edee
Akshay Sharma <akshay.sharma03@sap.com>
2021-12-16 09:33:10 +0530

    FreindsProfile Page Added

---
+++ lib/pages/Friendprofile_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+class FriendProfileWidget extends StatefulWidget {
+  FriendProfileWidget({Key key}) : super(key: key);
+
+  @override
+  _FriendProfileWidgetState createState() => _FriendProfileWidgetState();
+}
+
+class _FriendProfileWidgetState extends State<FriendProfileWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('FloatingActionButton pressed ...');
+        },
+        backgroundColor: Color(0xFFB39738),
+        elevation: 8,
+        child: Icon(
+          Icons.create_rounded,
+          color: Colors.white,
+          size: 24,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 3,
+                    color: Color(0x3A000000),
+                    offset: Offset(0, 1),
+                  )
+                ],
+              ),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 44, 0, 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                              ),
+                              child: Image.network(
+                                'https://i.picsum.photos/id/1074/5472/3648.jpg?hmac=w-Fbv9bl0KpEUgZugbsiGk3Y2-LGAuiLZOYsRk0zo4A',
+                                fit: BoxFit.fitHeight,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Rajat Ranjan',
+                            style: TextStyle(
+                              fontFamily: 'Lexend Deca',
+                              color: Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 0, 0, 12),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                            child: Text(
+                              'See my favorite picks.',
+                              style: TextStyle(
+                                fontFamily: 'Lexend Deca',
+                                color: Color(0xFF8B97A2),
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
